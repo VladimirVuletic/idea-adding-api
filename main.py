f0ca24a01694e8d734635d209065e689a791d90d
Vladimir Vuletic <vlado.vuletic@gmail.com>
2025-07-22 22:56:43 +0200

    fix typo

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ api = FastAPI()
 all_todos = [
     {"todo_id": 1, "todo_name": "Sports", "todo_description": "Go to the gym"},
     {"todo_id": 2, "todo_name": "Read", "todo_description": "Read ten pages"},
-    {"todo_id": 3, "todo_name": "Shop", "todo_description": "Go  shoppint"},
+    {"todo_id": 3, "todo_name": "Shop", "todo_description": "Go shopping"},
     {"todo_id": 4, "todo_name": "Study", "todo_description": "Study for exam"},
     {"todo_id": 5, "todo_name": "Meditate", "todo_description": "Meditate 20 minutes"},
 ]
