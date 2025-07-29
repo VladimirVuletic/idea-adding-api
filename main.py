628e3fac2c22ef428c58536275edb9bb14e66ed7
Vladimir Vuletic <vlado.vuletic@gmail.com>
2025-07-29 23:09:59 +0200

    add delete method. add change_file func

--- main.py
+++ main.py
@@ -25,6 +25,30 @@ def get_table():
 
     return json_table
 
+def change_file(json_table):
+    file_path = r"C:/Python projects/future-projects/README.md"
+    with open(file_path, 'r', encoding='utf-8') as file:
+        md_file = file.read()
+    soup = BeautifulSoup(md_file, 'html.parser')
+
+    table_body = soup.find('tbody')
+
+    new_tbody = soup.new_tag('tbody')
+    for row in json_table:
+        new_tr = soup.new_tag('tr')
+
+        for field in ("ID", "Project name", "Short description", "Long description"):
+            new_td = soup.new_tag('td')
+            new_td.append(row[field])
+            new_tr.append(new_td)
+        
+        new_tbody.append(new_tr)
+
+    table_body.replace_with(new_tbody)
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(str(soup))
+
 # GET, POST, PUT, DELETE
 # SIMPLE GET
 @api.get('/')
@@ -64,26 +88,7 @@ def create_idea(idea: dict):
 
     json_table.append(new_idea)
 
-    file_path = r"C:/Python projects/future-projects/README.md"
-    with open(file_path, 'r', encoding='utf-8') as file: md_file = file.read()
-    soup = BeautifulSoup(md_file, 'html.parser')
-
-    new_tr = soup.new_tag('tr')
-    for field in ("ID", "Project name", "Short description", "Long description"):
-        cell_value = new_idea[field]
-        td = soup.new_tag('td')
-        if '<' in cell_value and '>' in cell_value:
-            fragment = BeautifulSoup(cell_value, 'html.parser')
-            td.append(fragment)
-        else:
-            td.string = cell_value
-        new_tr.append(td)
-
-    table_body = soup.find('tbody')
-    table_body.append(new_tr)
-
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(str(soup))
+    change_file(json_table)
 
     return new_idea
 
@@ -97,14 +102,18 @@ def update_idea(idea_id: int, updated_idea: dict):
             idea['idea_description'] = updated_idea['idea_description']
             return idea
     return "Error, not found"
-
+"""
 
 # SIMPLE DELETE
 @api.delete('/ideas/{idea_id}')
 def delete_idea(idea_id: int):
+    json_table = get_table()
+
     for index, idea in enumerate(json_table):
-        if idea['idea_id'] == idea_id:
+        if idea['ID'] == str(idea_id):
             deleted_idea = json_table.pop(index)
+
+            change_file(json_table)
+
             return deleted_idea
     return "Error, not found"
-"""
