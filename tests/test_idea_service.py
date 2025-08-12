e4dcd3845e4abbaa5ac3dc21450ed5e8a867bda5
Vladimir Vuletic <vlado.vuletic@gmail.com>
2025-08-12 16:33:32 +0200

    implement test_find_idea_by_id_not_found

--- tests/test_idea_service.py
+++ tests/test_idea_service.py
@@ -9,7 +9,7 @@ def test_run_cmd():
 
 
 def test_find_idea_by_id_found():
-    # We can use parametrization here though
+    # TODO: We should use parametrization here though
     mock_ideas = [
         Idea(id='7', name='Name of idea 7', short_description='short desc for idea 7', long_description='long desc for idea 7'),
         Idea(id='X', name='Name of idea X', short_description='short desc for idea X', long_description='long desc for idea X'),
@@ -21,3 +21,9 @@ def test_find_idea_by_id_found():
     assert result.name == "Name of idea 7"
     assert result.short_description == "short desc for idea 7"
     assert result.long_description == "long desc for idea 7"
+
+
+def test_find_idea_by_id_not_found():
+    result = find_idea_by_id(id="7", ideas=[])
+
+    assert result == None
