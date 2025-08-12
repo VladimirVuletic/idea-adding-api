e3f1c14f005af52620f61411867be1dbde5a48d3
Vladimir Vuletic <vlado.vuletic@gmail.com>
2025-08-12 16:31:21 +0200

    implement test_find_idea_by_id_found

--- tests/test_idea_service.py
+++ tests/test_idea_service.py
@@ -1,4 +1,5 @@
-from app.services.idea_service import run_cmd
+from app.services.idea_service import run_cmd, find_idea_by_id
+from app.schemas.idea import Idea
 
 def test_run_cmd():
     cmd = ["cmd", "/c", "echo", "Something"]
@@ -6,3 +7,17 @@ def test_run_cmd():
 
     assert run_cmd(cmd, cwd) == "Something\n"
 
+
+def test_find_idea_by_id_found():
+    # We can use parametrization here though
+    mock_ideas = [
+        Idea(id='7', name='Name of idea 7', short_description='short desc for idea 7', long_description='long desc for idea 7'),
+        Idea(id='X', name='Name of idea X', short_description='short desc for idea X', long_description='long desc for idea X'),
+        Idea(id='   9\n', name='Name of idea 9\n', short_description='short desc for idea 9\n', long_description='long desc for idea 9\n'),
+    ]
+    result = find_idea_by_id(id="7", ideas=mock_ideas)
+
+    assert result.id == "7"
+    assert result.name == "Name of idea 7"
+    assert result.short_description == "short desc for idea 7"
+    assert result.long_description == "long desc for idea 7"
