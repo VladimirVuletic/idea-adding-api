fcaf203f859b85cb748a220482158a7e436ac2b5
Vladimir Vuletic <vlado.vuletic@gmail.com>
2025-08-17 22:04:53 +0200

    implement a simple version of IdeasFile

---
+++ app/utils/ideas_file.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+
+from app.core.settings import settings
+
+class IdeasFile:
+    file_path: str
+
+    def __init__(self, file_path: str = None):
+        self.file_path = file_path if file_path else settings.REPO_PATH + settings.FILE_NAME
+
+    def read_file(self):
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            md_file = file.read()
+
+        return md_file
+    
+    def write_to_file(self, soup: BeautifulSoup):
+        with open(self.file_path, 'w', encoding='utf-8') as file:
+            file.write(soup.prettify(formatter=None))
